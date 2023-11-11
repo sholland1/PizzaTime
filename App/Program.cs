02659ebf522eebd680fcfa4a28d0bb530e5a846e
Seth Holland <holland_s91@yahoo.com>
2023-11-10 19:21:02 -0600

    Add dependency injection

--- App/Program.cs
+++ App/Program.cs
@@ -1,26 +1,72 @@
-﻿using Controllers;
+﻿using System.Net;
+using CommandLine;
+using Controllers;
 using Hollandsoft.OrderPizza;
-using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenAI.Extensions;
+using Server;
+
+var provider = BuilderServiceProvider();
 
 AppDomain.CurrentDomain.UnhandledException += (_, args) => {
     var ex = (Exception)args.ExceptionObject;
-    Console.Error.WriteLine($"Unhandled exception: {ex.Message}");
-    Console.Error.WriteLine(ex.StackTrace);
+    provider.GetRequiredService<ILogger<Program>>()
+        .LogCritical(ex, "Unhandled exception");
     Environment.Exit(1);
 };
 
-var fastFlag = args.Contains("--fast");
+var controller = provider.GetRequiredService<PizzaController>();
 
-var pizzaController = new PizzaController(
-    new PizzaRepository(),
-    o => new DominosCart(
-        new DominosApi(new NullLogger<DominosApi>()),
-        o),
-    new RealConsoleUI());
+await Parser.Default.ParseArguments<CommandLineOptions>(args)
+    .WithParsedAsync(async o => {
 
-if (fastFlag) {
-    await pizzaController.FastPizza();
-    return;
+    if (o.Fast) {
+        await controller.FastPizza();
+        return;
+    }
+    _ = provider.GetRequiredService<PizzaQueryServer>().StartServer();
+    await controller.ShowOptions();
+});
+
+static ServiceProvider BuilderServiceProvider() {
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .AddUserSecrets<Program>()
+        .Build();
+
+    ServiceCollection services = new();
+    services.AddSingleton<IConfiguration>(configuration);
+
+    services.AddOpenAIService();
+
+    services.AddLogging()
+        .AddSingleton<IOrderApi, DominosApi>()
+        .AddSingleton<IPizzaRepo, PizzaRepository>()
+        .AddSingleton<Func<OrderInfo, ICart>>(services =>
+        o => new DominosCart(
+            services.GetRequiredService<IOrderApi>()!, o))
+
+        .AddSingleton(new AIPizzaBuilderConfig {
+            SystemMessageFile = "AIPizzaPromptSystemMessage.txt",
+            FewShotFile = "FewShotPrompt.json"
+        })
+        .AddSingleton<IAIPizzaBuilder, ChatCompletionsPizzaBuilder>()
+
+        .AddSingleton<IConsoleUI, RealConsoleUI>()
+        .AddSingleton<PizzaController>();
+
+    services.AddSingleton<PizzaQueryServer>(services =>
+        new(
+            IPAddress.Parse(configuration.GetValue<string>("PizzaQueryServer:Host")!),
+            configuration.GetValue<int>("PizzaQueryServer:Port"),
+            services.GetRequiredService<IPizzaRepo>()));
+
+    return services.BuildServiceProvider();
 }
 
-await pizzaController.ShowOptions();
+class CommandLineOptions {
+    [Option("fast", Required = false, HelpText = "Order the default pizza with user confirmation only.")]
+    public bool Fast { get; set; }
+}
